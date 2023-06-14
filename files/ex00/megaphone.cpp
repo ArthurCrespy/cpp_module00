dbe50138806e723cf7b6350346ea481fea36ac57
CRESPY Arthur <arthur@crespy.eu>
2023-06-14 13:42:58 +0200

    Add ex00

---
+++ files/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/02 12:27:02 by acrespy           #+#    #+#             */
+/*   Updated: 2023/06/03 09:10:47 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 0, j = 1; j < argc; i++, j++)
+			while (argv[j][i])
+				std::cout << (char)std::toupper(argv[j][i++]);
+	}
+	std::cout << std::endl;
+}
