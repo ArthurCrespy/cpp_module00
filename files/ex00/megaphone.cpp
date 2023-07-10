79ea34ca13631571631c34382876785ba1f22791
CRESPY Arthur <arthur@crespy.eu>
2023-07-10 14:34:31 +0200

    Fix multi-args issue

--- files/ex00/megaphone.cpp
+++ files/ex00/megaphone.cpp
@@ -14,13 +14,19 @@
 
 int main(int argc, char **argv)
 {
+	int i;
+
 	if (argc < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
-		for (int i = 0, j = 1; j < argc; i++, j++)
+		for (int j = 1; j < argc; j++)
+		{
+			i = 0;
 			while (argv[j][i])
 				std::cout << (char)std::toupper(argv[j][i++]);
+		}
+
 	}
 	std::cout << std::endl;
 }
